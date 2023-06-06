888cb2205a1729331fae3481c6024a51dd8ac825
Alberto Diaz Martin <adiazcan@hotmail.com>
2023-06-06 09:30:51 +0200

    initia version using GPT

---
+++ config.ts
@@ -0,0 +1,8 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  openAIKey: process.env.SECRET_OPENAI_API_KEY,
+  openAIEndpoint: process.env.OPENAI_ENDPOINT,
+};
+
+export default config;
